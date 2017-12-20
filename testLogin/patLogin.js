cc317cae62ce1eed9e08ee055e4bafded557e830
Shaun Bens <bensshaun@gmail.com>
2017-12-20 17:48:46 +0000

    initial setup

---
+++ testLogin/patLogin.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var patConfig = {
+    apiKey: "AIzaSyDAjzmDGWyJyGxQRtzCkntvgQkyuG-7xeA",
+    authDomain: "doctor-login-b6832.firebaseapp.com",
+    databaseURL: "https://doctor-login-b6832.firebaseio.com",
+    projectId: "doctor-login-b6832",
+    storageBucket: "doctor-login-b6832.appspot.com",
+    messagingSenderId: "819442752065"
+};
+firebase.initializeApp(patConfig);
